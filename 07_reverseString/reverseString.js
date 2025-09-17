745122b64adda787bcd496bb9a33553bb8c7db90
Sangeeth Suresh <sangeethsuresh321@gmail.com>
2025-09-17 13:59:56 +0000

    Complete exercise 7
    Completed exercise 7 using dedicated reverse for
    loop which was not required but is an alternate
    approach with more lines of code.

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  let len = str.length;
+  let arr = str.split('');
+  for (let i = 0; i < len / 2; i++) {
+    [arr[i], arr[len - i - 1]] = [arr[len - i - 1], arr[i]];
+  }
+  str = arr.join("");
+  return str;
+  // return str.split("").reverse().join(""); ALSO WORKS!!
 };
 
 // Do not edit below this line
