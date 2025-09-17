4f2abff56844f854bd8a54fbfa928b85fc59c50e
Sangeeth Suresh <sangeethsuresh321@gmail.com>
2025-09-17 15:31:36 +0000

    Complete 9th exercise
    Completed 9th exercise with least no. of lines.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function (min, max) {
+  if (!Number.isInteger(min) || !Number.isInteger(max) || min < 0 || max < 0)
+    return "ERROR";
+  let sum = 0;
+  if (min > max)
+    [min, max] = [max, min];
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
