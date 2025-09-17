99780bfbdd7f9d2b3adea41f398d529475ffdb0b
Sangeeth Suresh <sangeethsuresh321@gmail.com>
2025-09-17 15:58:57 +0000

    Complete 11th exercise
    Completed exercise 11 with two line solution
    instead of one liner. Understood one-liner.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahr) {
+  // let cel = (fahr - 32) * (5 / 9);
+  // let rounded = Math.round(cel * 10) / 10;
+  // return rounded;
+  return Math.round((fahr - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (cel) {
+  // let fahr = (9 / 5) * cel + 32;
+  // let rounded = Math.round(fahr * 10) / 10;
+  // return rounded;
+  return Math.round(((9 / 5) * cel + 32) * 10) / 10
 };
 
 // Do not edit below this line
