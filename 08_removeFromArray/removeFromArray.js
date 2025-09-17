23e5a4e38e03861a85cb1331dcc6cad0ae0bfcee
Sangeeth Suresh <sangeethsuresh321@gmail.com>
2025-09-17 14:22:39 +0000

    Complete exercise 8
    Completed exercise 8 with filter method but longer
    than required. Own solution given in comments.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,19 @@
-const removeFromArray = function() {
-};
+// const removeFromArray = function () {
+//   const args = [...arguments];
+//   let args2 = args.slice(1);
+//   let arr = args[0];
+//   return arr.filter((item) => {
+//     for (const i of args2) {
+//       if (i === item)
+//         return false;
+//     }
+//     return true;
+//   })
+// };
+
+const removeFromArray = function (array, ...args) {
+  return array.filter(val => !args.includes(val))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
