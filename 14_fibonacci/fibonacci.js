0557a326efdae80ee799d4ca7c6b1cae59d0a2a4
Sangeeth Suresh <sangeethsuresh321@gmail.com>
2025-09-25 15:47:00 +0000

    Complete fibonacci exercise
    Completed 14th fibonacci series exercise.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function (a) {
+  let num = +a;
+  if (num < 0)
+    return "OOPS";
+  else if (num == 0)
+    return 0;
 
+  let num1 = 1, num2 = 1;
+  if (num == 1 || num == 2)
+    return num1;
+  else {
+    let count = 2;
+    let num3;
+    while (count < num) {
+      num3 = num1 + num2;
+      num1 = num2;
+      num2 = num3;
+      count++;
+    }
+    return num3;
+  }
 };
 
 // Do not edit below this line
