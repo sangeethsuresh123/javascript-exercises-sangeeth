a97784d3ac616d245b3bbfb5b7959d6145fa537e
Sangeeth Suresh <sangeethsuresh321@gmail.com>
2025-09-25 16:51:35 +0000

    Complete 16th exercise
    Completed 16th find the oldest person from the
    people object array exercise.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,45 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  // return people.map(person => {
+  //   if (!('yearOfDeath' in person))
+  //     person['yearOfDeath'] = (new Date()).getFullYear();
+  //   person['age'] = person['yearOfDeath'] - person['yearOfBirth'];
+  //   return person;
+  // }).sort((a, b) => b.age - a.age)[0];
 
+  // people.map(person => {
+  //   if (!('yearOfDeath' in person))
+  //     person['yearOfDeath'] = (new Date()).getFullYear();
+  //   person['age'] = person['yearOfDeath'] - person['yearOfBirth'];
+  //   return person;
+  // });
+  // people.sort((a, b) => b.age - a.age);
+  // return people[0];
+  // let maxAge = 0;
+  // const oldestPerson = people.reduce((oldestPerson, person) => {
+  //   let age;
+  //   if ('yearOfDeath' in person) {
+  //     age = person['yearOfDeath'] - person['yearOfBirth'];
+  //     // console.log(age);
+  //   }
+  //   else
+  //     age = today - person['yearOfBirth'];
+  //   if (age > maxAge) {
+  //     maxAge = age;
+  //     console.log(maxAge);
+  //     oldestPerson = person;
+  //   }
+  //   console.log(oldestPerson);
+  //   return oldestPerson;
+  // }, {})
+  // return oldestPerson;
+
+  return people.sort((a, b) => getAge(b) - getAge(a))[0];
 };
+
+function getAge(person) {
+  person.yearOfDeath ??= new Date().getFullYear();
+  return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
