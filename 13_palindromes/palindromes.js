c76e97d43e271565afa24b39682fad579e65a5ee
Sangeeth Suresh <sangeethsuresh321@gmail.com>
2025-09-25 15:27:59 +0000

    Complete palindromes exercise
    Completed palindromes exercise with manual C-like
    solution not utilising js functions.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,33 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let arr = str.split(''); // check solution, this too long and manual not utilisings js functions
+  let arr2 = [];
+  for (let i of arr) {
+    if (isAlphabet(i) || isNumeric(i))
+      arr2.push(i);
+  }
+  let len = arr2.length;
+  for (let i = 0; i < len; i++) {
+    if (isUpperCase(arr2[i]))
+      arr2[i] = arr2[i].toLowerCase();
+  }
+  for (let i = 0; i < len / 2; i++) {
+    if (arr2[i] !== arr2[len - i - 1])
+      return false;
+  }
+  return true;
 };
+
+function isAlphabet(char) {
+  return char.toLowerCase() !== char.toUpperCase();
+}
+
+function isNumeric(char) {
+  return /^\d$/.test(char);
+}
+
+function isUpperCase(char) {
+  return char === char.toUpperCase();
+}
 
 // Do not edit below this line
 module.exports = palindromes;
