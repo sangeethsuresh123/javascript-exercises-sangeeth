002958eb0e6bd4cc3edd218a3cc1374990e6bcca
Sangeeth Suresh <sangeethsuresh321@gmail.com>
2025-09-25 15:58:07 +0000

    Complete titles exercise
    Completed 15th titles exercise using reduce
    function.

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function (books) {
+  let titles = [];
+  books.reduce((titles, item) => {
+    titles.push(item.title);
+    return titles;
+  }, titles);
+  console.log(titles);
+  return titles;
 };
 
 // Do not edit below this line
